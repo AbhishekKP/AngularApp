c0b589843c444e8f7045fe43b9f62e328d866eb4
abhishek <abhishek.kp@tecnotree.com>
2018-05-16 19:01:08 +0530

    dataBinding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,10 +1,17 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'app-server',
+    // selector: 'app-server', //This is selector with element we declare it as <app-server></app-server> 
+    selector: '[app-server]', //This is selector with attribute we declare it as <div app-server></div>
+    // selector: '.app-server', // This is selector with class we declare it as <div class="app-server"></div>
     templateUrl: './server.component.html'
 })
 
 export class ServerComponent {
+    serverID:number = 10;
+    serverStatus:string = 'Offline';
 
+    getServerStatus() {
+        return this.serverStatus;
+    };
 }
